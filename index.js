d25c8a262a5eb93891e032febcfdf2cc9cb023ab
Alexander Hamilton <aphamil06@gmail.com>
2024-07-30 20:41:54 -0500

    song complete and extracted repetitions into functions

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("hello world")
+song()
+function refrain() {
+    console.log("You're as smooth as Tennessee whiskey");
+console.log("You're as sweet as strawberry wine");
+console.log("You're as warm as a glass of brandy");
+console.log("and honey, i stay stoned on your love all the time");
+}
+function song(){
+// verse 1
+console.log("Used to spend my nights out in a barroom");
+console.log("Liquor was the only love i'd known");
+console.log("But you rescued me from reachin' for the bottom");
+console.log("And brought me back from being too far gone");
+
+refrain()
+
+// verse 2
+console.log("I've looked for love in all the same old places");
+console.log("Found the bottom of the bottle's always dry");
+console.log("But when you poured out your heart, I didn't waste it");
+console.log("'Cause there's nothing like your love to get me high");
+
+refrain()
+
+refrain()
+
+console.log("You're as smooth as Tennessee whiskey");
+console.log("Tennessee whiskey");
+console.log("Tennessee whiskey");
+
+console.log("You're as smooth as Tennessee whiskey");
+console.log("Tennessee whiskey");
+console.log("Tennessee whiskey");
+}
